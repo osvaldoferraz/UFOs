73ab050a19ce773ad1104c0a6b35d5192e041a47
Osvaldo Ferraz <osvaldoferraz@Osvaldos-MacBook-Pro.local>
2021-01-14 17:38:05 -0600

    11.5.2

---
+++ static/js/app.js
@@ -0,0 +1,18 @@
+// import data from data.js
+const tableData = data;
+// Reference the HTML table using d3
+var tbody = d3.select('tbody')
+
+// start of a new function and clear any existing data
+function buildTable(data) {
+   tbody.html("");
+    // Next, loop through each object in the data and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        //Loop through each field in the dataRow and add each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        })
+    });
+}
